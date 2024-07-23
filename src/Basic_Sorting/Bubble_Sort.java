8bd586e5020d3976f00ffb728c84ddaca247e6f6
Mohammed Afif <mohammedafif8951@gmail.com>
2024-07-23 20:16:20 +0530

    a

--- src/Basic_Sorting/Bubble_Sort.java
+++ src/Basic_Sorting/Bubble_Sort.java
@@ -36,11 +36,22 @@ public class Bubble_Sort {
             
         }
     }
+    public static void bubble_sort_descending(int arr[]){
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=0; j<arr.length-1-i; j++){
+                if(arr[j] < arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;                }
+            }
+        }
+    }
     public static void main(String[] args) {
         //int arr[] = {5,4,1,3,2};
         int arr[] = {1,2,5,4,3};
         //bubble_sort_Optimized(arr);
-        bubble_sort(arr);
+        // bubble_sort(arr);
+        bubble_sort_descending(arr);
         for(int a: arr){
             System.out.print(a+" ");
         }
