44b402df715f1dc0a381c74b67085e6b432c0955
Mohammed Afif <mohammedafif8951@gmail.com>
2024-07-23 20:06:00 +0530

    add

--- src/Basic_Sorting/Insertion_sort.java
+++ src/Basic_Sorting/Insertion_sort.java
@@ -1,4 +1,6 @@
-
+package Basic_Sorting;
+import java.util.Arrays;
+import java.util.Collections;
 public class Insertion_sort{
 
     public static void insertion_sort(int arr[]){
@@ -15,10 +17,21 @@ public class Insertion_sort{
             arr[prev+1] = cur;
         }
     }
+
+
     public static void main(String[] args) {
         int arr[] = {5,4,1,3,2};
-        insertion_sort(arr);
-        for(int a : arr){
+        // insertion_sort(arr);
+        // for(int a : arr){
+        //     System.out.print(a+" ");
+
+        
+        // Arrays.sort(arr);
+        // Arrays.sort(arr,0,3);
+        Integer arr1[] = {5,4,1,3,2};
+        // Arrays.sort(arr1,0,3,Collections.reverseOrder());
+        Arrays.sort(arr1,Collections.reverseOrder());
+        for(int a : arr1){
             System.out.print(a+" ");
         }
     }
---
+++ src/Basic_Sorting/countingSort.java
@@ -0,0 +1,37 @@
+package Basic_Sorting;
+
+public class countingSort {
+// find the largest element and create and count array of length largest+1 
+// iterate the given array for every element in the given array make count[arr[i]]++ i.e,
+// increase the count of that number
+// then iterate the count array and then for each count add the element to the given array
+    public static void counting_sort(int arr[]){
+        int largest = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+            largest = Math.max(largest, arr[i]);
+        }
+
+        //create and initialize counting arrray
+        int count[] = new int[largest+1];
+        for(int i=0; i<arr.length; i++){
+            count[arr[i]]++;
+        }
+
+        //sort 
+        int j=0;
+        for(int i=0; i<arr.length; i++){
+            while(count[i]>0){
+                arr[j] = i;
+                count[i]--;
+                j++;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,1,2,1,6,9,10};
+        counting_sort(arr);
+        for(int a : arr){
+            System.out.print(a+" ");
+        }
+    }
+}
